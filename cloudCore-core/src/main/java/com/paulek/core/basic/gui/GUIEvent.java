4d77419e90cd851135883fe1caf16b09a32d10eb
Paul <pawel.krzysztof.fortuna@outlook.com>
2019-08-10 12:14:05 +0200

    Modules? Bad idea

--- cloudCore-core/src/main/java/com/paulek/core/basic/gui/GUIEvent.java
+++
@@ -1,7 +0,0 @@
-package com.paulek.core.basic.gui;
-
-public interface GUIEvent<T> {
-
-    void event(T t);
-
-}
---
+++ src/main/java/com/paulek/core/basic/gui/GUIEvent.java
@@ -0,0 +1,7 @@
+package com.paulek.core.basic.gui;
+
+public interface GUIEvent<T> {
+
+    public void event(T t);
+
+}
